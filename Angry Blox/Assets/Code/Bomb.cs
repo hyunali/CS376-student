502ff2d7d2f547fc4198c922ff3496226e861f07
Najma Ali <najmaali2023@u.northwestern.edu>
2023-10-16 22:35:57 -0500

    went through and wrote the code for projectilethrower.cs, TargetBox.cs, and bomb.cs (****unity isn't working though so none of it has been tested)

--- Angry Blox/Assets/Code/Bomb.cs
+++ Angry Blox/Assets/Code/Bomb.cs
@@ -1,6 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Bomb : MonoBehaviour {
     public float ThresholdImpulse = 5;
     public GameObject ExplosionPrefab;
+
+    public BoxCollider2D boxCollider;
+    public SpriteRenderer spriteRenderer;
+    public PointEffector2D pointEffector;
+    public Bomb bomb;
+    
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        pointEffector = GetComponent<PointEffector2D>();
+        bomb = GetComponent<Bomb>();
+    }
+
+    void Destruct()
+    {
+        Destroy(gameObject);
+    }
+
+    void Boom()
+    {
+        pointEffector.enabled = true;
+        spriteRenderer.enabled = false;
+        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+        Invoke(nameof(Destruct), 0.1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D hit in collision.contacts)
+        {
+            if (hit.normalImpulse > ThresholdImpulse)
+            {
+                Boom();
+            }
+        }
+    }
 }
+
+
